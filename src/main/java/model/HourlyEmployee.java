54a182a927cb8e8d4dc8185e2d88bbc74a763fd2
Pradeep Sangli <pradeep.sangli@gmail.com>
2018-02-13 13:23:00 -0800

    fixed employeee

--- src/main/java/model/HourlyEmployee.java
+++ src/main/java/model/HourlyEmployee.java
@@ -1,18 +1,30 @@
 package model;
 
 public class HourlyEmployee extends Employee {
-    private double rate;
+    private double salary;   // Annual salary
 
-    public HourlyEmployee(String id, String firstName, String lastName, int sickDays, int vacationDays, double rate) {
-        super(id, firstName, lastName, sickDays, vacationDays);
-        this.rate = rate;
+    public HourlyEmployee(String name, String address, int ssn, int id, String method, double salary) {
+        super(name, address, ssn, id, method);
+        setSalary(salary);
     }
 
-    public double getRate() {
-        return rate;
+    public void mailCheck() {
+        System.out.println("Within mailCheck of Salary class ");
+        System.out.println("Mailing check to " + getName() + " with salary " + salary);
     }
 
-    public void setRate(double rate) {
-        this.rate = rate;
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double newSalary) {
+        if(newSalary >= 0.0) {
+            salary = newSalary;
+        }
+    }
+
+    public double computePay() {
+        System.out.println("Computing salary pay for " + getName());
+        return salary/52;
     }
 }
