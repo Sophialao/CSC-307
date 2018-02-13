6f5b29c6a590383a8253997b848ca13f40c754a9
SophiaLao <sophia.lao@gmail.com>
2018-02-13 12:13:24 -0800

    "commit"

--- src/main/java/model/Employee.java
+++ src/main/java/model/Employee.java
@@ -1,11 +1,34 @@
-
-public interface Employee
+abstract class Employee
 {
-    public String name;
-    public Date DOB;
-    public double curPay; //for current pay period
+    private String name;
+    private Date DOB;
+    private double curPay; //for current pay period
 
-    public void addEmplyee();
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public double getCurPay() {
+        return curPay;
+    }
+
+    public void setCurPay(double curPay) {
+        this.curPay = curPay;
+    }
+
+    public abstract void addEmplyee();
     public void deleteEmployee();
     public void updateEmployee();
     public Employee getEmployee();
