620233ce40bbec5dd7e72064a3d32b9aaf8056d3
Daniel Kim <phildankim@gmail.com>
2018-02-13 12:48:41 -0800

    added file parsing

--- src/main/java/util/Utils.java
+++ src/main/java/util/Utils.java
@@ -19,7 +19,7 @@ public class Utils {
 
                 StringBuilder builder = new StringBuilder();
                 for (int i = 1; i < split.length; i++)
-                    builder.append(split[i]);
+                    builder.append(split[i]+" ");
 
                 file.put(Integer.parseInt(split[0]), builder.toString());
             }
