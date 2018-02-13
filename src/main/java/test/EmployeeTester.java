96c21a5764d9a2566d42ffac07e9195e33a7d709
SophiaLao <sophia.lao@gmail.com>
2018-02-13 12:16:09 -0800

    hi

--- src/main/java/test/EmployeeTester.java
+++ src/main/java/test/EmployeeTester.java
@@ -1,4 +1,19 @@
 package test;
 
+import model.*;
+
 public class EmployeeTester {
+
+    public void testAddEmployee() {
+        Employee employee = new Employee();
+
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+        System.out.print("Hello!");
+    }
 }
