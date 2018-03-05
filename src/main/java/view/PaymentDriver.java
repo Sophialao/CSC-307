331b87fba3cc2f975fa7487c2fdc94f26f5df45a
Kelly Chen <kellychen@KellsMac.local>
2018-03-04 16:07:28 -0800

    Resets sales and added text message

--- src/main/java/view/PaymentDriver.java
+++ src/main/java/view/PaymentDriver.java
@@ -6,15 +6,12 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import static javafx.application.Application.launch;
-
 public class PaymentDriver extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("/PaymentView.fxml"));
 
         Scene scene = new Scene(root, 300, 275);
-
         stage.setTitle("Payment");
         stage.setScene(scene);
         stage.show();
