35c12451cc4ce8c7cd67296516e3a6dbe2e53f5c
Pradeep Sangli <pradeep.sangli@gmail.com>
2018-03-08 21:48:45 -0800

    sign in screen

--- src/main/java/view/LoginView.java
+++ src/main/java/view/LoginView.java
@@ -1,4 +1,26 @@
 package view;
 
-public class LoginView {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class LoginView extends Application{
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/LoginView.fxml"));
+
+        Scene scene = new Scene(root, 300, 275);
+        stage.setTitle("Employee");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
 }
